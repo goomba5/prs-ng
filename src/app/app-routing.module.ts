1d3276628120d91062150fb00d79e4c38aef05f5
Gary Henderson <hendergy@gmail.com>
2019-04-21 18:24:18 -0400

    add VendorListComponent route to the app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { UserListComponent } from "./feature/user/user-list/user-list.component";
 import { UserCreateComponent } from "./feature/user/user-create/user-create.component";
+import { VendorListComponent } from "src/app/feature/vendor/vendor-list/vendor-list.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/user/list", pathMatch: "full" },
   { path: "user/list", component: UserListComponent },
   { path: "user/create", component: UserCreateComponent },
+  { path: "vendor/list", component: VendorListComponent },
   { path: "**", component: UserListComponent }
 ];
 
